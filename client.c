b49904b950d32d460b46940682bc206e7d034103
Chalkiss <filgap@wp.pl>
2018-12-31 02:03:52 +0100

    Dodanie pierwowzoru interfejsu uzytkownika

--- client.c
+++ client.c
@@ -13,7 +13,7 @@
 #include "shm-wrappers.h"
 #include "sem-wrappers.h"
 #include "queue-wrappers.h"
-
+#include <ncurses.h>
 
 int main(int argc, char* argv[]){
     
@@ -27,13 +27,29 @@ int main(int argc, char* argv[]){
     player *rec = &buf;
     player_connected = att_shmem(shared_memory_id);
     *player_connected = 1;
-    printf("Polaczono z serwerem\n");
+
+    initscr();
+    mvprintw(4,5,"Resources: ");
+    mvprintw(7,5,"Workers: ");
+    mvprintw(10,5,"Units: ");
+    mvprintw(12,5,"Infantry: ");
+    mvprintw(15,5,"Heavy Infantry: ");
+    mvprintw(18,5,"Cavalry: ");
+    mvprintw(4,55,"Who to attack?");
+    mvprintw(13,55,"Victories: ");
     
+
+
+
+
+    refresh();   
     while(1){
         msg_received = receive_message(queue_id, rec, client_id+1);
         if(msg_received != -1){
             resources = rec->resources_amount;
-            printf("Ilosc surowcow: %d\n",resources);
+                mvprintw(5, 8,"%d", resources);
+                refresh();
             }
     }
+    return 0;
 }
